b6d84573fffabede525858186f62a53229f83abd
Abdelrhman Ezzat <abdelrhman18018692@gmail.com>
2024-09-04 19:59:23 +0300

    to change background & change icon

---
+++ 04 change background & icons.py
@@ -0,0 +1,11 @@
+from tkinter import *
+pro = Tk()
+pro.geometry('800x500') 
+################################
+## to change background >>
+pro.config(background='gray')
+################################
+## to change icon >>
+pro.iconbitmap(r'C:\Users\Al-arab\ICONS\studying.ico')
+################################
+pro.mainloop()
