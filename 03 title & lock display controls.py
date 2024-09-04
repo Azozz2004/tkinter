023ca03d207d0d8cce181502401582347c1a0c0b
Abdelrhman Ezzat <abdelrhman18018692@gmail.com>
2024-09-04 19:58:39 +0300

    to lock display controls & change title

---
+++ 03 title & lock display controls.py
@@ -0,0 +1,11 @@
+from tkinter import *
+pro = Tk()
+pro.geometry('800x500') 
+################################
+## to lock display controls >>
+pro.resizable(False , True)    # resizable("Width , Height")
+################################
+## to change title >>
+pro.title('The Title')
+################################
+pro.mainloop()
