eda845d0d7de9c79d233904fddd54193fe256afa
Abdelrhman Ezzat <abdelrhman18018692@gmail.com>
2024-09-04 19:57:40 +0300

    to show size of the window

---
+++ 02 size.py
@@ -0,0 +1,8 @@
+from tkinter import *
+pro = Tk()
+################################
+## 'Width x height + left place +  top place'  >>
+# pro.geometry('800x500')   # 'w X h'
+pro.geometry('800x500+500+200') 
+################################
+pro.mainloop()
