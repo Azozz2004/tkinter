edd8cb0b461722d544d582f7b6922a8b28f328d2
Abdelrhman Ezzat <abdelrhman18018692@gmail.com>
2024-09-04 19:59:54 +0300

    to min & max size of the window

---
+++ 05 min & max size.py
@@ -0,0 +1,8 @@
+from tkinter import *
+pro = Tk()
+pro.geometry('800x500') 
+################################
+pro.minsize(200,200)    # minsize(width , height)
+pro.maxsize(400,400)    # maxsize(width , height)
+################################
+pro.mainloop()
